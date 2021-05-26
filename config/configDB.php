11700be536447c310b1110bd0e48abdd65931575
Denis Dorogov <denisdorogov@yandex.ru>
2021-05-26 22:24:12 +0500

    Добавлены файлы с настройками и синглтон

---
+++ config/configDB.php
@@ -0,0 +1,9 @@
+<?php
+define("CONFIG_DB", [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'login' => 'root',
+    'password' => 'geekBrains7',
+    'database' => 'cj48357_math4kid',
+    'charset' => 'utf8'
+]);
