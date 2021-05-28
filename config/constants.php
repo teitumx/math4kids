177bed3a71e5d4fa82d96fade3c464b027ccb605
Denis Dorogov <denisdorogov@yandex.ru>
2021-05-28 22:07:41 +0500

    Добавил константу с параметрами уровня сложности.

---
+++ config/constants.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'difficulty' => [
+        'hard' => [
+            'summ' => [
+                'max' => 100,
+            ],
+            'diff' => [
+                'max' => 100,
+            ],
+            'mult' => [
+                'range' => [2 , 10],
+            ],
+            'div' => [
+                'range' => [2 , 10],
+            ]
+        ],
+        'easy' => [
+            'summ' => [
+                'max' => 20,
+            ],
+            'diff' => [
+                'max' => 20,
+            ],
+            'mult' => [
+                'range' => [2 , 5],
+            ],
+            'div' => [
+                'range' => [2 , 5],
+            ] 
+        ],
+    ],
+];
