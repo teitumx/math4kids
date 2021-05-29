e4a40f5e1241d2cf9bb1befdeb25ab2c2f0f4e4d
Denis Dorogov <denisdorogov@yandex.ru>
2021-05-29 14:07:05 +0500

    Сделал метод, возвращающий массив со всеми комбинациями примеров и ответов. Есть все элементы для внесения в таблицу .

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -28,4 +28,56 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
-}
+
+    public function createQuestions($difficulty) {
+        $arrayQuestions = [];
+        foreach ( $difficulty as $key => $value) {
+            for ($r = $value['summ'][1]; $r > $value['summ'][0]; $r--) { // a + b = r
+                $question = [];
+                for ( $a = $r - 1; $a > 0; $a--) {
+                    $b = $r - $a;
+                    $question[0] = $a . ' + '. $b; // text
+                    $question[1] = $r; // answer
+                    $question[2] = $key; // difficulty_type_id
+                    $question[3] =  'summ'; // type
+                    $arrayQuestions[] = $question;
+                };
+                
+            };
+            for ($a = $value['diff'][1]; $a > $value['diff'][0]; $a--) { // a - b = r
+                $question = [];
+                for ( $b = $a; $b > 0; $b--) {
+                    $question[0] = $a . ' - '. $b; // text
+                    $question[1] = $a - $b; // answer
+                    $question[2] = $key; // difficulty_type_id
+                    $question[3] =  'diff'; // type
+                    $arrayQuestions[] = $question;
+                };
+                
+            };
+            for ($a = $value['mult'][1]; $a >= $value['mult'][0]; $a--) { // a * b = r
+                $question = [];
+                for ( $b = 10; $b > 1; $b--) {
+                    $question[0] = $a . ' X '. $b; // text
+                    $question[1] = $a * $b; // answer
+                    $question[2] = $key; // difficulty_type_id
+                    $question[3] =  'mult'; // type
+                    $arrayQuestions[] = $question;
+                };
+                
+            };
+            for ($a = $value['div'][1]; $a >= $value['div'][0]; $a--) { // r / b = a
+                $question = [];
+                for ( $b = 10; $b > 1; $b--) {
+                    $r = $b * $a;
+                    $question[0] = $r . ' / '. $b; // text
+                    $question[1] = $a; // answer
+                    $question[2] = $key; // difficulty_type_id
+                    $question[3] =  'div'; // type
+                    $arrayQuestions[] = $question;
+                };
+            };
+        };
+        return $arrayQuestions;
+    };
+};
