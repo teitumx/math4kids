7c50e7779e89f9ea92ddb009e3e11b68dcb152b6
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-06-01 17:09:51 +0300

    feat: собрать проект

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/main.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/main.scss', 'public/css')
+    .options({
+        processCssUrls: false
+    })
     .copyDirectory('resources/views/image', 'public/image');
 
