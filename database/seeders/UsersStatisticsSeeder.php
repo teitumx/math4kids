f54309d87c7ea5bd5a351e26e3028e81e20cc8dc
Denis Dorogov <denisdorogov@yandex.ru>
2021-06-03 20:05:19 +0500

    Добавил миграцию и сидер UserStatistics таблицы

---
+++ database/seeders/UsersStatisticsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Statistics;
+use Illuminate\Database\Seeder;
+
+class UsersStatisticsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
