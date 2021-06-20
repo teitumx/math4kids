c2a4833a67326dcb204ab655d6565e3a170fe638
Michese <michese48@mail.ru>
2021-06-20 14:53:13 +0300

    Удалил лишние правила для UserAnswerRequest

--- app/Http/Requests/UserAnswerRequest.php
+++ app/Http/Requests/UserAnswerRequest.php
@@ -10,12 +10,6 @@ class UserAnswerRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name' => 'required|string|min:2|max:127',
-            'last_name' => 'required|string|min:2|max:127',
-            'login' => 'required|string|unique:users|min:4|max:20',
-            'password' => 'required|string|confirmed|min:4|max:20',
-            'birthday' => 'required|date',
-            'avatar' => 'nullable',
             'number' => 'required|integer',
             'question_id' => 'required|integer',
             'answer' => 'required|string',
